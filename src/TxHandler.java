0431e0ba853f982b01943611713ca2bc82880ab2
zhaizhonghao <390930230@qq.com>
2019-08-16 17:49:59 +0800

    TxHandler.java changed

--- src/TxHandler.java
+++ src/TxHandler.java
@@ -11,7 +11,7 @@ public class TxHandler {
 	private UTXOPool utxoPool;
     public TxHandler(UTXOPool utxoPool) {
         // IMPLEMENT THIS
-    	utxoPool = new UTXOPool(utxoPool);
+    	this.utxoPool = new UTXOPool(utxoPool);
     }
 
     /**
@@ -24,11 +24,10 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // IMPLEMENT THIS
     	double sumOfInputValue = 0;
     	double sumOfOutputValue = 0;
-    	Set<UTXO> used = new HashSet<>();
-    	for (int i = 0; i < tx.getInputs().size(); i++) {
+    	UTXOPool uniqueUtxos = new UTXOPool();
+    	for (int i = 0; i < tx.numInputs(); i++) {	
 			//ith input claimed by {@code tx} is in the current UTXO pool,
     		Transaction.Input input= tx.getInput(i);
     		UTXO utxo = new UTXO(input.prevTxHash,input.outputIndex);
@@ -43,12 +42,13 @@ public class TxHandler {
 				return false;
 			}
     		//no UTXO is claimed multiple times by {@code tx},
-    		if (!used.add(utxo)) {
+    		if (uniqueUtxos.contains(utxo)) {
 				return false;
 			}
+    		uniqueUtxos.addUTXO(utxo, output);
 		}
     	// all of {@code tx}s output values are non-negative
-    	for (int i = 0; i < tx.getOutputs().size(); i++) {
+    	for (int i = 0; i < tx.numOutputs(); i++) {
     		Transaction.Output output = tx.getOutput(i);
     		
     		if (output.value < 0) {
@@ -74,36 +74,27 @@ public class TxHandler {
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
     	HashSet<Transaction> txVis = new HashSet<>();
-    	//使用while的原因是，因为处理的是无序的交易，所以可能处理某些交易后，之前失败的交易又可以成功了
-    	while(true){
-    		boolean updated = false;
-	    	for (int i = 0; i < possibleTxs.length; i++) {
-				Transaction transaction = possibleTxs[i];
-				if (txVis.contains(transaction)) {
-					continue;
+    	for (int i = 0; i < possibleTxs.length; i++) {
+			Transaction transaction = possibleTxs[i];
+			if (txVis.contains(transaction)) {
+				continue;
+			}
+			if (isValidTx(transaction)) {
+				txVis.add(transaction);
+				//remove the UTXOs corresponding to inputs of transaction
+				for (int j = 0; j < transaction.numInputs(); j++) {
+					Transaction.Input input = transaction.getInput(j);
+					UTXO utxo = new UTXO(input.prevTxHash,input.outputIndex);
+					utxoPool.removeUTXO(utxo);
 				}
-				if (isValidTx(transaction)) {
-					txVis.add(transaction);
-					updated = true;
-					//remove the UTXOs corresponding to inputs of transaction
-					for (int j = 0; j < transaction.numInputs(); j++) {
-						Transaction.Input input = transaction.getInput(j);
-						UTXO utxo = new UTXO(input.prevTxHash,input.outputIndex);
-						utxoPool.removeUTXO(utxo);
-					}
-					//add the UTXOs corresponding to outputs of transaction
-					for (int j = 0; j < transaction.numOutputs(); j++) {
-						Transaction.Output output = transaction.getOutput(j);
-						UTXO utxo = new UTXO(transaction.getHash(),j);
-						utxoPool.addUTXO(utxo, output);
-					}
+				//add the UTXOs corresponding to outputs of transaction
+				for (int j = 0; j < transaction.numOutputs(); j++) {
+					Transaction.Output output = transaction.getOutput(j);
+					UTXO utxo = new UTXO(transaction.getHash(),j);
+					utxoPool.addUTXO(utxo, output);
 				}
 			}
-	    	if (!updated) {
-				break;
-			}
-    	}
-    	
+		}
         Transaction[] ret = new Transaction[txVis.size()];
         int idx =0;
         for(Transaction tx : txVis)
